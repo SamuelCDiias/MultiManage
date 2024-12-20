7a74a37e89228300bc09bacd47c4fcec40448497
Samuel Dias <samuelcarvalhodias2004@gmail.com>
2024-12-20 15:40:25 -0300

    023

--- resources/views/livewire/sidebar/logout.blade.php
+++ resources/views/livewire/sidebar/logout.blade.php
@@ -1,6 +1,10 @@
 <div>
-    <button wire:click='logout'
-                    class="w-full bg-red-500 px-4 py-2 text-white rounded-lg hover:bg-red-600 transition duration-200">
-                    Sair
-                </button>
+    <button wire:click="logout" class="p-3 bg-gray-800 text-white rounded-full hover:bg-gray-700 m-4 mt-auto">
+        <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd"
+                d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
+                clip-rule="evenodd"></path>
+        </svg>
+        <span>Logout</span>
+    </button>
 </div>
